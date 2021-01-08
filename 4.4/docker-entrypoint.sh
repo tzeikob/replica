9ec3d9a4423e596fc2a93c2fe7f54f138e6765f7
Iakovos Papadopoulos <iakopap@gmail.com>
2021-01-08 18:50:50 +0200

    Launch daemon as mongodb user

--- 4.4/docker-entrypoint.sh
+++ 4.4/docker-entrypoint.sh
@@ -2,6 +2,32 @@
 set -eo pipefail
 shopt -s nullglob
 
-echo "Entering the entry point script"
+# Skip if we aren't running mongo related binaries
+if [[ "$1" == mongo* ]]; then
+  echo "Entrypoint script for MongoDB Server ${MONGO_VERSION} started"
+
+  # Declare data directory path
+  declare -g DATADIR
+  DATADIR="/data/db"
+
+  # Change data directory with permissions for mongodb user on running as root
+  if [ "$(id -u)" = "0" ]; then
+    echo "Switching to mongodb user"
+
+    # Todo: Maybe catch this if running "mongod" only
+    find $DATADIR \! -user mongodb -exec chown mongodb '{}' +
+
+    # Make sure we can write to stdout and stderr as mongodb
+    chown --dereference mongodb "/proc/$$/fd/1" "/proc/$$/fd/2" || :
+
+    exec gosu mongodb "$BASH_SOURCE" "$@"
+  fi
+
+  # Use numactl to start your mongod, config servers and mongos
+  numa='numactl --interleave=all'
+  if $numa true &> /dev/null; then
+    set -- $numa "$@"
+  fi
+fi
 
 exec "$@"
