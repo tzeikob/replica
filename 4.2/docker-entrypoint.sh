fe370536f4dc884b70f01671dea646958dda23f0
Iakovos Papadopoulos <iakopap@gmail.com>
2021-02-12 20:40:27 +0200

    Add dockerfile files for older mongodb versions

---
+++ 4.2/docker-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -eo pipefail
+shopt -s nullglob
+
+# Append any given mongod configuration argument
+if [ "${1:0:1}" = '-' ]; then
+  set -- mongod "$@"
+fi
+
+# Skip if we aren't running mongo related binaries
+if [[ "$1" == mongo* ]]; then
+  echo "Entrypoint script for MongoDB Server ${MONGO_VERSION} started"
+
+  # Change data directory with permissions for mongodb user on running as root
+  if [ "$(id -u)" = "0" ]; then
+    echo "Switching to mongodb user"
+
+    # Update data and config folder ownership
+    find $MONGO_DATA_HOME \! -user mongodb -exec chown mongodb '{}' +
+    find $MONGO_CONFIG_HOME \! -user mongodb -exec chown mongodb '{}' +
+
+    # Make sure we can write to stdout and stderr as mongodb
+    chown --dereference mongodb "/proc/$$/fd/1" "/proc/$$/fd/2" || :
+
+    # Execute mongod or mongos as mongodb user
+    exec gosu mongodb "$BASH_SOURCE" "$@" --bind_ip_all
+  fi
+
+  # Use numactl to start your mongod, config servers and mongos
+  numa='numactl --interleave=all'
+  if $numa true &> /dev/null; then
+    set -- $numa "$@"
+  fi
+fi
+
+exec "$@"
